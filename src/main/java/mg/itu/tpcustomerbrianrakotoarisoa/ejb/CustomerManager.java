ed372483e28d0b839539de81a4bd23e7db8d1a6b
Brian <brianrakotoarisoa@gmail.com>
2023-03-28 12:12:27 +0300

    Ajout d'une page JSF pour afficher la liste des clients

--- src/main/java/mg/itu/tpcustomerbrianrakotoarisoa/ejb/CustomerManager.java
+++ src/main/java/mg/itu/tpcustomerbrianrakotoarisoa/ejb/CustomerManager.java
@@ -15,20 +15,25 @@ import mg.itu.tpcustomerbrianrakotoarisoa.entities.Customer;
  *
  * @author Brian Rakotoarisoa
  */
+
+/**
+ * Gère la persistance des Customers.
+ */
 @Stateless
 public class CustomerManager {
+
     @PersistenceContext(unitName = "customerPU")
-    private EntityManager em;    
-    
-    public List<Customer> getAllCustomers(){
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
        Query query = em.createNamedQuery("Customer.findAll");
        return query.getResultList();
     }
-    
-    public Customer update(Customer customer){
-        return em.merge(customer);
+
+    public Customer update(Customer customer) {
+       return em.merge(customer);
     }
-    
+
     public void persist(Customer customer) {
        em.persist(customer);
     }
